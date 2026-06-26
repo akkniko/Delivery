eef34a77618eb7773954b57b19e90fe648b6ac81
artem <nukonopov@gmail.com>
2026-06-26 17:35:34 +0300

    fix all bugs< program is working now

--- src/DeliveryService.java
+++ src/DeliveryService.java
@@ -1,15 +1,14 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class DeliveryService{
     private ArrayList<Courier> couriers = new ArrayList<>();
     private ArrayList<Order> orders = new ArrayList<>();
+    private HashMap<Integer, Courier> map = new HashMap<>();
+
 /*
 Метод регистрации курьера: принимает объект курьера и добавляет его в общий список.
-
 Метод создания заказа: принимает вес и стоимость, создает объект заказа, добавляет в список заказов. Сразу после этого запускает внутренний метод поиска курьера.
-
 Метод назначения курьера: перебирает список курьеров. Ищет того, кто: а) свободен, б) его CourierType позволяет поднять вес этого заказа (проверка лимитов 5 кг или 30 кг). Если нашли — меняем статус заказа на DELIVERING, курьера делаем занятым. Если не нашли — выводим сообщение «Нет подходящих курьеров».
-
 Метод завершения доставки: принимает ID заказа. Находит этот заказ, меняет статус на DELIVERING -> DELIVERED. Находит курьера, который его вез, начисляет ему на баланс, например, 10% от стоимости заказа, и переводит курьера в статус «свободен».
 */
     
@@ -25,8 +24,6 @@ public class DeliveryService{
     Courier findCourier(Order o){
         for(Courier c : couriers){
             if(o.getWeight() <= c.getType().getMaxW() && !c.isBusy()){
-                curRegistration(c);
-                o.setStatus(OrderStatus.DELIVERING);
                 return c;
             }
         }
@@ -35,26 +32,39 @@ public class DeliveryService{
 
     void makeOrder(double price, double w, int id, String name){
         Order o = new Order(id, name, w,price);
+        Courier c;
         if(!orders.contains(o)){
             orders.add(o);
-            findCourier(o);
-            o.setStatus(OrderStatus.CREATED);
+            c = findCourier(o);  //назначаю курьера на этот заказ
+            if(c != null){
+                c.setIsBusy(true);  //меняю его флаг занятости
+                o.setStatus(OrderStatus.DELIVERING);
+                map.put(o.getId(), c);
+            }
+            else{
+                o.setStatus(OrderStatus.CREATED);
+            }
         }
         else{
             System.out.println("This order is already exist! ");
         }
     }
 
+
     void destinDelivery(int id){
         //todo: exceptions
-        Courier c;
-        for(Order o: orders){
+        for(Order o : orders){
             if(id == o.getId()){
                 o.setStatus(OrderStatus.DELIVERED);
-                System.out.println("The order has been delivered");
-                c = findCourier(o);
-                c.setIsBusy(false);
-                c.increaseCash(o.getPrice()/10);
+                Courier c = map.get(o.getId());
+                if(c != null){
+                    c.changeBStatus();
+                    c.increaseCash(o.getPrice()/10);
+                }
+                else{
+                    System.out.println("The order hadn't had an assigned courier ");
+                }
+                break;
             }
         }
     }
