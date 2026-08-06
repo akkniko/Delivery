2f0d1e0a2eb205a011f6995bed747ffed6ec1d4a
artem <nukonopov@gmail.com>
2026-08-06 14:20:52 +0300

    rename: some methods

--- src/main/java/com/company/delivery/service/DeliveryService.java
+++ src/main/java/com/company/delivery/service/DeliveryService.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 import com.company.delivery.model.Courier;
 import com.company.delivery.model.Order;
@@ -13,7 +14,7 @@ import com.company.delivery.model.OrderStatus;
 public class DeliveryService{
     private final List<Courier> couriers = new ArrayList<>();
     private final List<Order> orders = new ArrayList<>();
-    private final HashMap<Integer, Courier> map = new HashMap<>();
+    private final Map<Integer, Courier> assignedCourier = new HashMap<>();
 
 /*
 Метод регистрации курьера: принимает объект курьера и добавляет его в общий список.
@@ -22,7 +23,7 @@ public class DeliveryService{
 Метод завершения доставки: принимает ID заказа. Находит этот заказ, меняет статус на DELIVERING -> DELIVERED. Находит курьера, который его вез, начисляет ему на баланс, например, 10% от стоимости заказа, и переводит курьера в статус «свободен».
 */
 
-    public void curRegistration(Courier cr){
+    public void registerCourier(Courier cr){
         if(couriers.contains(cr)){
             System.out.println("this courier is already in list");
         }
@@ -39,7 +40,7 @@ public class DeliveryService{
                 .findFirst();
     }
 
-    public Order makeOrder(double price, double w, String name) {
+    public Order createOrder(double price, double w, String name) {
         Order o = new Order(name, w, price);
         if(!orders.contains(o)) orders.add(o);
 
@@ -49,7 +50,7 @@ public class DeliveryService{
                     t -> {
                         t.markBusy();
                         o.setStatus(OrderStatus.DELIVERING);
-                        map.put(o.getId(), t);
+                        assignedCourier.put(o.getId(), t);
                         System.out.println("order " + o.getName() + "  delivering! ");
                     },
                     () -> {
@@ -61,7 +62,7 @@ public class DeliveryService{
         return o;
     }
 
-    public void destinDelivery(int id) {
+    public void completeDelivery(int id) {
         //todo: exceptions, stream api
         orders.stream().
                 filter(t -> t.getId() == id)
@@ -71,7 +72,7 @@ public class DeliveryService{
                     c.setStatus(OrderStatus.DELIVERED);
                     System.out.println("Order " + c.getName() + " id: " + c.getId() + " delivered");
 
-                    Courier k = map.get(c.getId());
+                    Courier k = assignedCourier.get(c.getId());
                     if(k != null) {
                         k.markFree();
                         k.increaseCash(c.getPrice() / 10);
